ca41c3d610f823460c9e5cdb9454f1a56f753c11
Anday Ismayilzade <ipman4819@gmail.com>
2020-02-17 23:25:21 +0400

    advantages section added

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,39 @@
 let turner = document.getElementById("turner");
+let turner2 = document.getElementById("turner2");
 let translateVal = 0;
-function divider(event){
-    tagRemove();
-    let element = event.toElement;
-    if(element.id == "div1")
-            translateVal = 0;      
+function divider(event) {
+        let dividers = document.getElementById("dividers");
+        tagRemove(dividers);
+        let element = event.toElement;
+        if (element.id == "div1")
+                translateVal = 0;
 
-    if(element.id == "div2")
-            translateVal = -100; 
+        if (element.id == "div2")
+                translateVal = -100;
 
-    if(element.id == "div3")
-            translateVal = -200; 
-    element.classList.add("selected-divider");
-    turner.style.transform = "translate(" + translateVal.toString() + "vw)";
-    turner.style.transition = "transform 1s";
+        if (element.id == "div3")
+                translateVal = -200;
+        element.classList.add("selected-divider");
+        turner.style.transform = "translate(" + translateVal.toString() + "vw)";
+        turner.style.transition = "transform 1s";
 }
 
-function tagRemove(){
-    let dividers = document.getElementById("dividers"); 
-    for(let i = 1; i < dividers.childNodes.length; i+=2)
-            dividers.childNodes[i].classList.remove("selected-divider");
+
+function divider2(event) {
+        let dividers = document.getElementById("dividers2");
+        tagRemove(dividers);
+        let element = event.toElement;
+        if (element.id == "div1")
+                translateVal = 0;
+
+        if (element.id == "div2")
+                translateVal = -1170;
+        element.classList.add("selected-divider");
+        turner2.style.transform = "translate(" + translateVal.toString() + "px)";
+        turner2.style.transition = "transform 1s";
+}
+
+function tagRemove(dividers) {
+        for (let i = 1; i < dividers.childNodes.length; i += 2)
+                dividers.childNodes[i].classList.remove("selected-divider");
 }
